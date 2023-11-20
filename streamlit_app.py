7c4d676954dcbe7a783900be3236792b4320e80e
Rohini Ahire <48978056+rohini-31@users.noreply.github.com>
2023-11-20 17:28:43 +0530

    Update streamlit_app.py
    new comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text(' 🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+  
